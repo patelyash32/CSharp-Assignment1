fc3324141049a1502f1abc90a6e559cc014d63b8
HP <HP@DESKTOP-E7H3AEU>
2024-03-13 00:40:55 +0530

    Add project files.

---
+++ CSharp-Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using CSharp_Assignment1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student1 = Student.CreateStudent("1", StudentType.FullTime, "John", "Anderson");
+        var student2 = Student.CreateStudent("2", StudentType.FullTime, "Karen", "Smith");
+        var student3 = Student.CreateStudent("3", StudentType.PartTime, "Kevin", "Peters");
+
+        Console.WriteLine("Assignment One:");
+        Student.OutputStudent(student1);
+        Student.OutputStudent(student2);
+        Student.OutputStudent(student3);
+    }
+}
